2e901f4078a0bc02f3861a4d16220f8ef0c26264
Jonathan Loescher <jonathan@jloescher.com>
2023-03-21 23:21:19 -0400

    Updated to title and content from name and body Added simple validation to prevent blank form submission Added Date Updated post component

--- src/components/createpostform.js
+++ src/components/createpostform.js
@@ -1,38 +1,49 @@
 import { useState } from "react";
 
 function CreatePostForm({ onSubmit }) {
-  const [name, setName] = useState("");
-  const [body, setBody] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    onSubmit({ name, body });
-    setName("");
-    setBody("");
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate that title and content are not empty
+    if (!title.trim() || !content.trim()) {
+      return;
+    }
+
+    const newPost = {
+      title,
+      content,
+      date: Date.now()
+    };
+    onSubmit(newPost);
+    setTitle('');
+    setContent('');
+  };
 
   return (
     <form onSubmit={handleSubmit} className="max-w-md mx-auto">
       <div className="mb-4">
-        <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
-          Name
+        <label htmlFor="title" className="block text-gray-700 font-bold mb-2">
+          Title
         </label>
         <input
           type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
       <div className="mb-6">
-        <label htmlFor="body" className="block text-gray-700 font-bold mb-2">
-          Body
+        <label htmlFor="content" className="block text-gray-700 font-bold mb-2">
+          Content
         </label>
         <textarea
-          id="body"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         ></textarea>
       </div>
@@ -48,5 +59,4 @@ function CreatePostForm({ onSubmit }) {
   );
 }
 
-
-export default CreatePostForm
+export default CreatePostForm;
--- src/components/post.js
+++ src/components/post.js
@@ -10,8 +10,9 @@ const Post = ({ post }) => {
 
   return (
     <div className="border border-gray-400 p-4 rounded-md mb-4">
-      <h2 className="text-lg font-semibold mb-2">{post.name}</h2>
-      <p className="text-gray-700">{post.body}</p>
+      <h2 className="text-lg font-semibold mb-2">{post.title}</h2>
+      <p className="text-gray-700">{post.content}</p>
+      <p className="text-gray-500 mt-2">{new Date(post.date).toLocaleDateString()}</p>
       <LikeDislikeButtons liked={liked} onClick={handleLikeClick} className="mt-4" />
     </div>
   );
